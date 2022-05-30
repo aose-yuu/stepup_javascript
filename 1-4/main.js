02e801dea7c3ac4e067dae7c3e848f565dfdf1fa
aose-yuu <aose.developer@gmail.com>
2022-05-31 03:19:12 +0900

    Add: ログに時刻とメッセージを表示させる

---
+++ 1-4/main.js
@@ -0,0 +1,37 @@
+function addMessage(message) {
+  var messageElement = document.createElement('div');
+  var now = new Date();
+  messageElement.innerText = now.getHours() + '時' + now.getMinutes() + '分' + now.getSeconds() + '秒 ' + message;
+  messageElement.classList = ['message'];
+  logElement.appendChild(messageElement);
+}
+
+var displayElement = document.getElementsByClassName('display')[0];
+var logElement = document.querySelector('.log');
+var timer = null;
+var seconds = 0;
+
+var startButton = document.getElementsByClassName('start-button')[0];
+startButton.addEventListener('click', function() {
+  if (timer === null) {
+    console.log('start');
+    timer = setInterval(function() {
+      seconds++;
+      displayElement.innerText = seconds;
+      console.log(seconds);
+    }, 1000);
+
+    addMessage('開始');
+  }
+});
+
+var stopButton = document.getElementsByClassName('stop-button')[0];
+stopButton.addEventListener('click', function() {
+  if (timer !== null) {
+    clearInterval(timer);
+    timer = null;
+    seconds = 0;
+    displayElement.innerText = seconds;
+    addMessage('終了')
+  }
+});
