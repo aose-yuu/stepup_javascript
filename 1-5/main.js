203219fe396f6db845077973871ef105fc694762
aose-yuu <aose.developer@gmail.com>
2022-05-31 04:32:03 +0900

    同じ画面に4つのストップウォッチを表示できるように

--- 1-5/main.js
+++ 1-5/main.js
@@ -1,4 +1,4 @@
-function stopWatch(options) {
+function stopWatch(i, options) {
   function addMessage(message) {
     var messageElement = document.createElement('div');
     var now = new Date();
@@ -17,32 +17,32 @@ function stopWatch(options) {
   options = options || {};
   var color = options.color || 'lightblue';
   var backgroundColor = options.backgroundColor || 'black';
-  var displayElement = document.getElementsByClassName('display')[0];
+  var displayElement = document.getElementsByClassName('display')[i];
   displayElement.style.color = color;
   displayElement.style.backgroundColor = backgroundColor;
 
-  var logElement = document.querySelector('.log');
+  var logElement = document.querySelectorAll('.log')[i];
   var timer = null;
   var seconds = 0;
 
-  var startButton = document.getElementsByClassName('start-button')[0];
+  var startButton = document.getElementsByClassName('start-button')[i];
   startButton.addEventListener('click', function() {
     startButton.disabled = true;
     stopButton.disabled = false;
     if (timer === null) {
-      console.log('start');
+      console.log(i + ': start');
       timer = setInterval(function() {
         seconds++;
         displayElement.innerText = seconds;
         ramenTimer(seconds);
-        console.log(seconds);
+        console.log(i + ': ' + seconds);
       }, 1000);
 
       addMessage('開始');
     }
   });
 
-  var stopButton = document.getElementsByClassName('stop-button')[0];
+  var stopButton = document.getElementsByClassName('stop-button')[i];
   stopButton.disabled = true;
   stopButton.addEventListener('click', function() {
     startButton.disabled = false;
@@ -61,4 +61,7 @@ var options = {
   color: 'limegreen',
   backgroundColor: '#333'
 };
-stopWatch(options);
+
+for (var i = 0; i < 4; i++) {
+  stopWatch(i, options);
+}
