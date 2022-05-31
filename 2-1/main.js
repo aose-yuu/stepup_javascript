c2b267d47bd0573990043274e88731652ae96988
aose-yuu <aose.developer@gmail.com>
2022-05-31 10:25:37 +0900

    Fix: varをconst/letに変更

---
+++ 2-1/main.js
@@ -0,0 +1,67 @@
+function stopWatch(i, options) {
+  function addMessage(message) {
+    const messageElement = document.createElement('div');
+    const now = new Date();
+    messageElement.innerText = now.getHours() + '時' + now.getMinutes() + '分' + now.getSeconds() + '秒 ' + message;
+    messageElement.classList = ['message'];
+    logElement.insertAdjacentElement('afterbegin', messageElement);
+  }
+
+  function ramenTimer(seconds) {
+    const oneMinutes = 60;
+    if (seconds === oneMinutes * 3) {
+      alert('ラーメンができました！');
+    }
+  }
+
+  options = options || {};
+  const color = options.color || 'lightblue';
+  const backgroundColor = options.backgroundColor || 'black';
+  const displayElement = document.getElementsByClassName('display')[i];
+  displayElement.style.color = color;
+  displayElement.style.backgroundColor = backgroundColor;
+
+  const logElement = document.querySelectorAll('.log')[i];
+  let timer = null;
+  let seconds = 0;
+
+  const startButton = document.getElementsByClassName('start-button')[i];
+  startButton.addEventListener('click', function() {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    if (timer === null) {
+      console.log(i + ': start');
+      timer = setInterval(function() {
+        seconds++;
+        displayElement.innerText = seconds;
+        ramenTimer(seconds);
+        console.log(i + ': ' + seconds);
+      }, 1000);
+
+      addMessage('開始');
+    }
+  });
+
+  const stopButton = document.getElementsByClassName('stop-button')[i];
+  stopButton.disabled = true;
+  stopButton.addEventListener('click', function() {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    if (timer !== null) {
+      clearInterval(timer);
+      timer = null;
+      seconds = 0;
+      displayElement.innerText = seconds;
+      addMessage('終了')
+    }
+  });
+}
+
+const options = {
+  color: 'limegreen',
+  backgroundColor: '#333'
+};
+
+for (var i = 0; i < 4; i++) {
+  stopWatch(i, options);
+}
